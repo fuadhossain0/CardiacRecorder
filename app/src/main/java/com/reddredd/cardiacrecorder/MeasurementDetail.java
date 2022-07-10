4f9313a49a2ea95b9f233cf72b1ded68d54a81f3
Waliul(Laptop) <00waliul00@gmail.com>
2022-07-10 22:23:06 +0600

    added recycler view

---
+++ app/src/main/java/com/reddredd/cardiacrecorder/MeasurementDetail.java
@@ -0,0 +1,83 @@
+package com.reddredd.cardiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MeasurementDetail extends AppCompatActivity {
+
+    private TextView sysOut, diasOut, heartOut, dateOut, timeOut, commentOut;
+    MaterialButton editBtn, deleteBtn;
+    int id = 0;
+    Bundle extras;
+    LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurement_detail);
+        extras = getIntent().getExtras();
+        initWidgets();
+        setMeasurements();
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EditMeasurement.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Delete Successful!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+    }
+
+    private void initWidgets() {
+        linearLayout = findViewById(R.id.detailsLayout);
+        sysOut = findViewById(R.id.sysOutput);
+        diasOut = findViewById(R.id.diasOutput);
+        heartOut = findViewById(R.id.heartOutput);
+        dateOut = findViewById(R.id.dateOutput);
+        timeOut = findViewById(R.id.timeOutput);
+        commentOut = findViewById(R.id.commentOutput);
+        editBtn = findViewById(R.id.editB);
+        deleteBtn = findViewById(R.id.deleteB);
+    }
+
+    private void setMeasurements() {
+        if(extras != null)
+            id = extras.getInt("id");
+
+        for(Measurement mes : Measurement.measurementArrayList)
+        {
+            if(mes.getId() == id)
+            {
+                sysOut.setText(mes.getSystolicPressure());
+                diasOut.setText(mes.getDiastolicPressure());
+                heartOut.setText(mes.getHeartRate());
+                dateOut.setText(mes.getDate());
+                timeOut.setText(mes.getTime());
+                commentOut.setText(mes.getComment());
+                int sys, dia;
+                sys = Integer.parseInt(mes.getSystolicPressure());
+                dia = Integer.parseInt(mes.getDiastolicPressure());
+                if(sys < 90 || sys > 140 || dia < 60 || dia > 90)
+                    linearLayout.setBackgroundResource(R.drawable.rounded_corner_red);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/reddredd/cardiacrecorder/recyclerAdapter.java
@@ -0,0 +1,78 @@
+package com.reddredd.cardiacrecorder;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.MyViewHolder> {
+
+    private ArrayList<Measurement> measurementList;
+    private RecyclerViewClickListener listener;
+
+    public recyclerAdapter(ArrayList<Measurement> measurementList, RecyclerViewClickListener listener){
+        this.measurementList = measurementList;
+        this.listener = listener;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView sysOut, diasOut, heartOut, dateOut, timeOut;
+        LinearLayout bg;
+
+        public MyViewHolder(final View view){
+            super(view);
+
+            bg = view.findViewById(R.id.bg_item);
+            sysOut = view.findViewById(R.id.sysOutput);
+            diasOut = view.findViewById(R.id.diasOutput);
+            heartOut = view.findViewById(R.id.heartOutput);
+            dateOut = view.findViewById(R.id.dateOutput);
+            timeOut = view.findViewById(R.id.timeOutput);
+
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onClick(view, getAdapterPosition());
+        }
+    }
+
+    @NonNull
+    @Override
+    public recyclerAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyclerAdapter.MyViewHolder holder, int position) {
+        holder.sysOut.setText(measurementList.get(position).getSystolicPressure());
+        holder.diasOut.setText(measurementList.get(position).getDiastolicPressure());
+        holder.heartOut.setText(measurementList.get(position).getHeartRate());
+        holder.dateOut.setText(measurementList.get(position).getDate());
+        holder.timeOut.setText(measurementList.get(position).getTime());
+
+        int sys, dia;
+        sys = Integer.parseInt(measurementList.get(position).getSystolicPressure());
+        dia = Integer.parseInt(measurementList.get(position).getDiastolicPressure());
+        if(sys < 90 || sys > 140 || dia < 60 || dia > 90)
+            holder.bg.setBackgroundResource(R.drawable.rounded_corner_red);
+    }
+
+    @Override
+    public int getItemCount() {
+        return measurementList.size();
+    }
+
+    public interface RecyclerViewClickListener {
+        void onClick(View v, int position);
+    }
+}
